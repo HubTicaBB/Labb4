5ef0093a4f8f1526da2ec26125c5453f997617ba
Andreea Nenciu <andreeanen@yahoo.com>
2019-09-27 15:58:50 +0200

    Added properties and costructor to Player class

--- Labb4/Labb4/Player.cs
+++ Labb4/Labb4/Player.cs
@@ -2,5 +2,17 @@
 {
     internal class Player
     {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        int StartPositionRow = 3;
+        int StartPositionColumn = 3;
+
+        public Player(string name)
+        {
+            this.Name = name;
+        }
+
+
+
     }
 }
