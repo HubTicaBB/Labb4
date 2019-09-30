63c5b11bb237c4472d6b702c1740013bbc25506a
Andreea Nenciu <andreeanen@yahoo.com>
2019-09-30 12:13:30 +0200

    Add map start layout and method print map

--- Labb4/Labb4/Player.cs
+++ Labb4/Labb4/Player.cs
@@ -1,22 +1,64 @@
 ﻿using System;
 
 namespace Labb4
+
 {
     internal class Player
     {
         public string Name { get; set; }
         public int Score { get; set; }
-        int StartPositionRow = 3;
-        int StartPositionColumn = 3;
+        int startPositionRow = 3;
+        int startPositionCol = 3;
+
+        char[,] mapLayout = new char[,]
+
+        {
+                {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+                {'#', '-', '-', '-','-','-', '-', '-', '-', '#'},
+                {'#', '-', '-', '-','-','-', '-', '-', '-', '#'},
+                {'#', '-', '-', '-','-','-', '-', '-', '-', '#'},
+                {'#', '-', '-', '-','-','-', '-', '-', '-', '#'},
+                {'#', '-', '-', '-','-','-', '-', '-', '-', '#'},
+                {'#', '-', '-', '-','-','-', '-', '-', '-', '#'},
+                {'#', '-', '-', '-','-','-', '-', '-', '-', '#'},
+                {'#', '-', '-', '-','-','-', '-', '-', '-', '#'},
+                {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+        };
 
         public Player(string name, int score = 100)
         {
             this.Name = name;
         }
 
-        internal void Play()
+        public static void StartMap(char[,] map, int startPositionRow, int startPositionCol)
         {
-            throw new NotImplementedException();
+            for (int row = 0; row < map.GetLength(0); row++)
+            {
+                for (int col = 0; col < map.GetLength(1); col++)
+                {
+
+                    if ((row == startPositionRow) && (col == startPositionCol))
+                        map[row, col] = '@';
+                }
+            }
+            PrintMap(map);
+        }
+        public static void PrintMap(char[,] map)
+        {
+            for (int row = 0; row < map.GetLength(0); row++)
+            {
+                for (int col = 0; col < map.GetLength(1); col++)
+                {
+                    Console.Write(map[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void Play()
+        {
+            StartMap(mapLayout, startPositionRow, startPositionCol);
+            Console.WriteLine("play");
         }
     }
 }
