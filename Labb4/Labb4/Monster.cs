c83e38a65e0be3c960a8eb011637168c09769ae5
Tijana <tijana.ilic87@protonmail.com>
2019-10-02 16:51:08 +0200

    Revert "Add FightMonster(), change CreateObjects()"
    This reverts commit b4de27349d87081578e1a7ebf228bc595c79f491.

--- Labb4/Labb4/Monster.cs
+++ Labb4/Labb4/Monster.cs
@@ -1,50 +1,13 @@
 ﻿using System;
-using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Labb4
 {
-    class Monster : IAvailable
+    class Monster
     {
-        public int MonsterPower { get; set; }
-        static Game game = new Game();
-        static Player player = game.players[game.players.Count - 1];
-        public Monster()
-        {
-            MonsterPower = 10;
-        }
-        public void FightMonster(Items weapon)
-        {
-            Console.WriteLine("You reached a room with a terryfing monster!! Use your weapons to fight the beast!");
-            string input = Console.ReadLine();
-
-            if (weapon.GetType().Name.ToString() == input)
-            {
-                MonsterPower -= 5;
-                player.itemsList.Remove(weapon);
-            }
-            else if (weapon.GetType() == typeof(Sword))
-            {
-                MonsterPower -= 2;
-            }
-        }
-        public bool IsBoxAvailable()
-        {
-            foreach (var item in player.itemsList)
-            {
-                if (item.GetType() == typeof(Bomb) || item.GetType() == typeof(Sword))
-                {
-                    FightMonster(item);
-                    item.NumberUsageKey--;
-                    if (item.NumberUsageKey == 0)
-                    {
-                        player.itemsList.Remove(item);
-                    }
-                    return true;
-                }
-            }
-            Console.WriteLine("You have no weapons to fight the monster. Go around and pick up some!");
-            Thread.Sleep(1000);
-            return false;
-        }
+        public int Power { get; set; }        
     }
 }
